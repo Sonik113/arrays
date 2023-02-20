607a46c12b2768dbdbe16ce312b5b271d5bb4465
Sonik113 <123419503+Sonik113@users.noreply.github.com>
2023-02-21 00:04:00 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+
+            //int[] array1 = new int[4];
+
+            //for (int i = 0; i < array1.Length; i++)
+            //{
+            //    Console.Write(array1[i] + " ");
+            //}
+
+            //Задание 2
+
+            //int[] array2 = new int[] { 8, 4, 7, 9, 1, 6 };
+
+            //for (int i = 0; i < array2.Length; i++)
+            //{
+            //    Console.Write(array2[i] + " ");
+            //}
+
+            //Задание 3
+
+            
+        }
+    }
+}
